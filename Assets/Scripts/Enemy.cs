e451927740c4f603ba5c9b6a9fc95cbbab852803
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2026-07-26 20:53:34 +1000

    Detection and movement working

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,6 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-	[SerializeField] private ViewCone v;
 
 	[Header("Alertness")]
 	private float alertness;
@@ -15,13 +14,21 @@ public class Enemy : MonoBehaviour
 
 	[Header("Movement")]
 	private NavMeshAgent agent;
-	[SerializeField] private Transform[] waypoints;
 	private int waypointIndex;
 	private Vector3 lastKnownPlayerLocation;
+	[SerializeField] private Transform[] waypoints;
 
 
 	[Header("Behaviour")]
-	[SerializeField] private float alertnessFollowPercentage;
+	[SerializeField, Range(0, 1)] private float alertnessFollowPercentage;
+	[SerializeField, Range(0, 1)] private float alertnessCuriousPercentage;
+	private bool isChasing;
+
+	[Header("Debug")]
+	[SerializeField] private Transform testCollision;
+
+	[Header("Viewcones")]
+	[SerializeField] private ViewCone[] viewcones;
 	private void Awake()
 	{
 		agent = GetComponent<NavMeshAgent>();
@@ -30,52 +37,43 @@ public class Enemy : MonoBehaviour
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
 	{
-
+		waypointIndex = GetNearestWaypoint();
+		agent.SetDestination(waypoints[waypointIndex].position);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		ProcessAlertness(v.InCone(transform, GameManager.Instance.Player.transform.position));
-
-		
+		ProcessAlertness(CheckPlayerVisible());
+		ProcessMovement();
 	}
 
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <param name="playerInView"></param>
-	/// <returns>True if player is </returns>
-	bool ProcessAlertness(bool playerInView)
+	bool CheckPlayerVisible()
 	{
-		if (!playerInView) // NEEDS ACTUAL CODE, add raycast for hiding?
+		foreach (ViewCone vc in viewcones)
 		{
-			// Player not visible
-			// If alertness is at 0, just break here.
-			if (alertness <= 0) return true;
-
-
-			if (alertDecay > 0)
+			// In cone, successful raycast and has component
+			if (vc.InCone(transform, GameManager.Instance.Player.transform.position) &&
+				Physics.Raycast(transform.position, GameManager.Instance.Player.transform.position - transform.position, out RaycastHit hit, vc.length)
+				&& hit.transform.TryGetComponent(out PlayerTopDown p))
 			{
-				// Start decreasing the decay timer
-				alertDecay -= Time.deltaTime;
-			}
-			else
-			{
-				// Decay time has reached zero, start decreasing the alertness
-				alertness -= Time.deltaTime * alertnessDecreaseRate;
-				if (alertness <= 0)
-				{
-					return true;
-				}
+				// Player in line of sight
+				Debug.Log("PLAYER IN VIEW!!");
+				return true;
 			}
 		}
-		else
+		return false;
+	}
+
+	void ProcessAlertness(bool playerInView)
+	{
+		if (playerInView)
 		{
 			// Player visible
 			// Set decay to max;
 			alertDecay = alertDecayDuration;
-
+			// Mark last known location
+			lastKnownPlayerLocation = GameManager.Instance.Player.transform.position;
 			// Increase alertness
 			alertness += Time.deltaTime * alertnessIncreaseRate;
 			if (alertness >= 1)
@@ -83,26 +81,138 @@ public class Enemy : MonoBehaviour
 				GameManager.Instance.EndGame(false);
 			}
 		}
-		return false;
+		else
+		{
+			// Player not visible
+			if (alertDecay > 0)
+			{
+				// Start decreasing the decay timer
+				alertDecay -= Time.deltaTime;
+			}
+			else if (alertness > 0)
+			{
+				// Decay time has reached zero, start decreasing the alertness
+				alertness -= Time.deltaTime * alertnessDecreaseRate;
+			}
+			else
+			{
+				// alertness at zero
+				// Forget player location
+				lastKnownPlayerLocation = Vector3.negativeInfinity;
+			}
+		}
 	}
 
 	void ProcessMovement()
 	{
-		// Recently saw player
-		if (alertDecay > 0 && alertness > )
+		if (alertDecay > 0 && lastKnownPlayerLocation != Vector3.negativeInfinity)// && alertness > alertnessFollowPercentage)
+		{
+			//switch (alertness)
+			//{
+			//	case float i when i > alertnessFollowPercentage:
+			//		break;
+			//	case float i when i > alertnessCuriousPercentage:
+			//		break;
+			//	default:
+			//		break;
+			//}
+			//// Can currently see player
+			//// Recently saw player
+			//if (alertness > alertnessFollowPercentage)
+			//{
+			//}
+
+			// Move to last known player position
+			// Only update if the last know location has changed
+			if (agent.destination != lastKnownPlayerLocation)
+			{
+				agent.SetDestination(lastKnownPlayerLocation);
+				// Just to ensure the position isn't off the navmesh
+				lastKnownPlayerLocation = agent.destination;
+			}
+		}
+		else
+		{
+			if (isChasing)
+			{
+				// Was chasing player last frame
+				// Change destination to closest waypoint
+				waypointIndex = GetNearestWaypoint();
+				agent.SetDestination(waypoints[waypointIndex].position);
+				isChasing = false;
+			}
+			else
+			{
+				if (Vector3.Distance(transform.position, waypoints[waypointIndex].position) <= agent.stoppingDistance)
+				{
+					// Increment waypoint path
+					waypointIndex = (waypointIndex + 1) % waypoints.Length;
+					agent.SetDestination(waypoints[waypointIndex].position);
+				}
+			}
+		}
+
+
 	}
 
-	public bool PlayerInViewCone => v.InCone(transform, new PlayerTopDown().transform.position);
+	int GetNearestWaypoint()
+	{
+		float shortestDistance = Mathf.Infinity;
+		int shortestIndex = -1;
+		NavMeshPath path = new();
+
+		for (int i = 0; i < waypoints.Length; i++)
+		{
+			// Calculate the path to the point
+			NavMesh.CalculatePath(transform.position, waypoints[i].position, NavMesh.AllAreas, path);
+
+			// Calculate the path length
+			float distance = 0;
+			for (int j = 0; j < path.corners.Length - 1; j++)
+			{
+				distance += Vector3.Distance(path.corners[j], path.corners[j + 1]);
+			}
+			// Compare to the current shortest path
+			if (distance < shortestDistance)
+			{
+				// Point is closer
+				shortestDistance = distance;
+				shortestIndex = i;
+			}
+		}
+		return shortestIndex;
+	}
 
 #if UNITY_EDITOR
 	private void OnDrawGizmos()
 	{
-		// Render behind objects 
-		Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
-		v.DebugDraw(transform, detectTf.position, 0.5f);
-		// Render in front of objects 
-		Handles.zTest = UnityEngine.Rendering.CompareFunction.Greater;
-		v.DebugDraw(transform, detectTf.position, 0.1f);
+		// Render viewcone
+		if (testCollision)
+		{
+			foreach (ViewCone vc in viewcones)
+			{
+				// Render behind objects 
+				Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
+				vc.DebugDraw(transform, testCollision.transform.position, 0.5f);
+				// Render in front of objects 
+				Handles.zTest = UnityEngine.Rendering.CompareFunction.Greater;
+				vc.DebugDraw(transform, testCollision.transform.position, 0.1f);
+			}
+		}
+
+		// Render waypoint path
+		Gizmos.color = Color.yellow;
+		for (int i = 0; i < waypoints.Length; i++)
+		{
+			Gizmos.DrawLine(waypoints[i].position, waypoints[(i + 1) % waypoints.Length].position);
+		}
+
+		//Render current path
+		if (agent)
+		{
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawLine(transform.position, agent.destination);
+		}
 	}
 #endif
 }
